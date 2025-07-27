5e9fe545119da7ac40a9b085a2e6b6f2d221fb37
ZaXaR <zaxar1990jem@gmail.com>
2025-07-27 15:17:55 +0300

    user services get/update profile

---
+++ src/services/user.service.ts
@@ -0,0 +1,32 @@
+import { axiosAuth } from "@/api/interceptor";
+import { IUser } from "@/interfaces/auth.interface";
+
+export interface iProfileResponse {
+    user: IUser;
+}
+
+class UserService {
+    private BASE_URL = "/api/user";
+
+    async getProfile(): Promise<iProfileResponse> {
+        const response = await axiosAuth.get<iProfileResponse>(`${this.BASE_URL}/profile`);
+
+        if (!response.data) {
+            throw new Error("Failed to fetch profile");
+        }
+
+        return response.data;
+    }
+
+    async updateProfile(data: Partial<IUser>): Promise<iProfileResponse> {
+        const response = await axiosAuth.put<iProfileResponse>(`${this.BASE_URL}/profile`, data);
+
+        if (!response.data) {
+            throw new Error("Failed to update profile");
+        }
+
+        return response.data;
+    }
+}
+export const userService = new UserService();
+export default userService;
