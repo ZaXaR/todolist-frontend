79716ad445d9cdc5088a8074e019373b9a941ad2
ZaXaR <zaxar1990jem@gmail.com>
2025-07-27 20:59:16 +0300

    master merge

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { EnumToken } from "./enums/auth.enum";
+
+export async function middleware(
+    request: NextRequest,
+    response: NextResponse
+) {
+
+    const token = request.cookies.get(EnumToken.ACCESS_TOKEN)?.value;
+
+    const isDashboardPage = request.url.includes("/dashboard");
+    const isAuthPage = request.url.includes("/auth");
+
+    if (!token && isDashboardPage) {
+        return NextResponse.redirect(new URL("/auth", request.url));
+    }
+
+    if (token && isAuthPage) {
+        return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+
+    return NextResponse.next();
+
+}
